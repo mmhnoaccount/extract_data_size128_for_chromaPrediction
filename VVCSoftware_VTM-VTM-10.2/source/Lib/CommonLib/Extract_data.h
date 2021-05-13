c66b29f2c3688ec703f3ebd7ffb3fbc2fd91fc85
mominghong <1512415885@qq.com>
2021-05-13 15:21:49 +0800

    extract_data_128_Chroma

---
+++ VVCSoftware_VTM-VTM-10.2/source/Lib/CommonLib/Extract_data.h
@@ -0,0 +1,18 @@
+
+#ifndef _EXTRACT_DATA_H_
+#define _EXTRACT_DATA_H_
+
+#define EXTRACT_DATA      1
+
+//#include <ostream>
+//#include <fstream>
+#include "CommonDef.h"
+
+//#include <windows.h>
+//#include <time.h>
+#include <sys/timeb.h>
+
+#define extract_128X128     1
+
+
+#endif
